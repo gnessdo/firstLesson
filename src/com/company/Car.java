0a4e1dceb0eb5d0b85a0ede5f54e2b6e0dd6883f
Ayxan <gnessdo@gmail.com>
2021-10-15 15:54:56 +0400

    new lesson

---
+++ src/com/company/Car.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Car{
+
+    public void func1(){
+        System.out.println("Masinlar siyahisi");
+    }
+
+    public void toplama(int x, int y){
+        System.out.println("Cem: "+(x+y));
+    }
+
+    public String istifadeciAdi(){
+        return "Salam Istifadeci";
+    }
+
+    public int vurma(int x, int y){
+        int hasil = x*y;
+        return hasil;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Car kenan = new Car();
+//        Car ayxan = new Car();
+//
+//        kenan.func1();
+//        ayxan.toplama(5,8);
+//
+//        String gelenAd = kenan.istifadeciAdi();
+//        System.out.println(gelenAd);
+//
+//        System.out.println("-----------------");
+//
+//        int cavab = kenan.vurma(8,10);
+//        System.out.println(cavab);
+
+        Main mn = new Main();
+        mn.toplama();
+
+    }
+
+
+    public void toplama(){
+        System.out.println("blalabala");
+    }
+}
